2af2bdacef3aaf5dd000a867d10d4372f80c002f
zerocalorie1226 <kjyy07@naver.com>
2022-12-27 13:46:57 +0900

    [생성]환경설정 구성

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    borwser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: {},
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
