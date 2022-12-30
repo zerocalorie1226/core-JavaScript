7d0af189d43183201867b06b2ea67a4c1acef63f
zerocalorie1226 <kjyy07@naver.com>
2022-12-30 09:38:51 +0900

    [수정] userName 코드 수정

--- client/chapper/core/08-2.condition.js
+++ client/chapper/core/08-2.condition.js
@@ -30,15 +30,16 @@ let whichTruthy = false || '' || [2, 3].length || { thisIsTruthy: true };
 
 let userName = prompt('사용자 아이디를 입력해주세요.', '');
 
-if (userName === 'Admin') {
+// console.log(userName);
+if (userName?.toLowerCase() === 'admin') {
   let pw = prompt('비밀번호를 입력해 주세요.', '');
 
-  if (pw === 'TheMaster') {
+  if (pw?.toLowerCase() === 'themaster') {
     console.log('환영합니다.');
   } else {
     console.log('취소되었습니다.');
   }
-} else if (userName === '' || userName === null) {
+} else if (userName.replace(/\s*/g, '') === '' || userName === null) {
   console.log('취소했습니다.');
 } else {
   console.log('인증되지 않은 사용자 입니다.');
