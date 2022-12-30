a215b58183e2e4950e76db9f4ba140bdf50e57d3
zerocalorie1226 <kjyy07@naver.com>
2022-12-30 13:02:27 +0900

    [추가] do..while문 학습

---
+++ client/chapper/core/09-1.loop.js
@@ -0,0 +1,81 @@
+/* ---------------------------------------------------------------------- */
+/* While Loop                                                             */
+/* ---------------------------------------------------------------------- */
+
+/* 
+let i = 0;
+
+while(i < 10){
+  console.log(++i);
+
+  
+}
+
+ */
+
+const frontEndDev = [
+  'HTML',
+  'CSS',
+  'SVG',
+  'JavaScript',
+  'jQuery',
+  'React',
+  'Redux',
+];
+
+/* 프론트엔드 개발 집합 항목 출력 ---------------------------------------------- */
+
+// console.log(frontEndDev[0]);
+// console.log(frontEndDev[1]);
+// console.log(frontEndDev[2]);
+// console.log(frontEndDev[3]);
+// console.log(frontEndDev[4]);
+// console.log(frontEndDev[5]);
+
+let z1 = performance.now();
+
+let i = 0;
+
+while (i < frontEndDev.length) {
+  let value = frontEndDev[i];
+
+  // console.log(i);
+
+  console.log(value);
+
+  i++;
+}
+
+let z2 = performance.now();
+
+console.log(z2 - z1);
+
+/* 
+let i = frontEndDev.length;
+
+while( i >= 0){
+
+  console.log(i, frontEndDev[i] );
+
+  i--
+}
+
+ */
+
+// 전개 연산자  전개 구문 spread oparator
+
+// let copyArray = [...frontEndDev];
+
+// let copyArray = frontEndDev.slice();
+
+// while(copyArray.length){
+//   console.log(copyArray.pop());
+// }
+
+/* 프론트엔드 개발 집합을 순환해서 각 아이템을 Console 패널에 출력 -------------------- */
+
+// while 문 (순환 : 순방향)
+
+// while 문 (역순환 : 역방향)
+
+// 성능 진단 : 순환 vs. 역순환
---
+++ client/chapper/core/09-2.loop.js
@@ -0,0 +1,51 @@
+/* ---------------------------------------------------------------------- */
+/* Do While Loop                                                          */
+/* ---------------------------------------------------------------------- */
+
+// let i = 10;
+
+// do{
+
+//   console.log('최초 선언');
+
+//   i++;
+
+// }while(false)
+
+// do ~ while 문 (역순환)
+
+// - prompt 창을 띄워 사용자로 하여금 순환 횟수를 요청
+// - 사용자로부터 요청된 횟수 만큼 역방향으로 순환 출력
+// - 사용자로부터 요청된 횟수가 0보다 작을 경우,
+//   '최초 실행된 메시지입니다. 이 메시지는 조건이 거짓이어도 볼 수 있습니다.' 출력
+// - 순환 중단
+
+/* let count = prompt('몇 번 순환할까요?','');
+
+do{
+  
+ 
+
+  if(count <= 0 || !count ){
+    console.log('최초실행');
+    break;
+  }
+
+  count--;
+  console.log(count);
+
+}while(count)
+
+
+ */
+
+// do ~ while 문 (순환)
+// - 위 do ~ while 문을 순방향으로 순환되도록 설정
+
+let first = document.querySelector('.first');
+
+do {
+  first = first.nextSibling;
+} while (first.nodeType !== document.ELEMENT_NODE);
+
+console.log(first);
