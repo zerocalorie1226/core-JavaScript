d4a7cec4a264cc47bf4ecd573e151f3e62321076
zerocalorie1226 <kjyy07@naver.com>
2023-01-12 09:03:31 +0900

    [추가] 버블링

--- client/chapper/dom/06.event.js
+++ client/chapper/dom/06.event.js
@@ -12,6 +12,8 @@
 
 const first = getNode('.first');
 const second = getNode('.second');
+const ground = getNode('.ground');
+const ball = getNode('.ball');
 
 function handler() {
   console.log('hit !');
@@ -19,11 +21,56 @@ function handler() {
   // css('.second','display','none')
 }
 
-// first.addEventListener('click', handler);
+first.addEventListener('click', handler);
 
-const off = bindEvent('.first', 'click', handler);
+// const off = bindEvent('.first','click',handler);
+// bindEvent('.second','click',off);
 
-bindEvent('.second', 'click', off);
+ground.addEventListener('click', function (e) {
+  console.log(e.offsetX, e.offsetY);
+
+  ball.style.transform = `translate(${e.offsetX - ball.offsetWidth / 2}px,${
+    e.offsetY - ball.offsetHeight / 2
+  }px)`;
+});
+
+function debounce(callback, limit = 100) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      callback.apply(this, args);
+    }, limit);
+  };
+}
+
+function throttle(callback, limit = 100) {
+  let waiting = false;
+  return function () {
+    if (!waiting) {
+      callback.apply(this, arguments);
+      waiting = true;
+      setTimeout(() => {
+        waiting = false;
+      }, limit);
+    }
+  };
+}
+
+// resize
+ground.addEventListener(
+  'mousemove',
+  debounce((e) => {
+    console.log(e.offsetX, e.offsetY);
+
+    let posX = e.offsetX;
+    let posY = e.offsetY;
+
+    const emotion = /* html */ `<div class="emotion" style="left:${posX}px;top:${posY}px">😘</div>`;
+
+    insertLast(ground, emotion);
+  }, 1000)
+);
 
 // second.addEventListener('click',function(){
 //   first.removeEventListener('click', handler);
