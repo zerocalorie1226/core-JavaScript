9f12a3ac8461d1c38c7822a96de190070e413986
zerocalorie1226 <kjyy07@naver.com>
2023-01-16 11:32:17 +0900

    [추가] 배열 구조 분해 할당

--- client/main.js
+++ client/main.js
@@ -1,7 +1,51 @@
-import { diceAnimation, getNode } from './lib/index.js';
+import { diceAnimation, getNode, getNodes } from './lib/index.js';
 
-const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+// [ 주사위 굴리기 ]
+// 1. dice 애니메이션 불러오기
+// 2. bindEvent 유틸 함수 만들기
+// 3. handleRollingDice 함수 만들고 토글로 애니메이션 제어하기
+// 4. 변수 보호를 위한 클로저 + IIFE 사용하기
 
-const handlerRollingDice = () => {};
+// [ 레코드 리스트 보이기 ]
+// 1. handleRecord 함수를 만들기
+// 2. disable 활성 유틸 함수 만들기
+// 3. handleReset 함수를 만듭니다.
+// 4. visible 활성 유틸 함수 만들기
+// 5. toggleState 유틸 함수 만들기
+
+// 배열의 구조 분해 할당
+const [rollingDiceButton, recordButton, resetButton] = getNodes(
+  '.buttonGroup > button'
+);
+
+// console.log(button);
+// const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+// const recordButton = getNode('.buttonGroup > button:nth-child(2)');
+// const resetButton = getNode('.buttonGroup > button:nth-child(3)');
+
+// let stopAnimation;
+
+// IIFE
+
+const handlerRollingDice = (() => {
+  let isRolling = false;
+  let stopAnimation;
+
+  return () => {
+    if (!isRolling) {
+      // console.log('첫번째 클릭');
+      stopAnimation = setInterval(diceAnimation, 100);
+      recordButton.disabled = true;
+    } else {
+      // console.log('두번째 클릭');
+      clearInterval(stopAnimation);
+      recordButton.disabled = false;
+    }
+
+    isRolling = !isRolling;
+  };
+})();
 
 rollingDiceButton.addEventListener('click', handlerRollingDice);
+
+// let eventOff = bindEvent(rollingDiceButton,'click',handlerRollingDice);
