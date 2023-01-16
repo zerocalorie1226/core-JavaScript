a62373c39abadec3ae88c3c9a996052ebc7617a3
zerocalorie1226 <kjyy07@naver.com>
2023-01-16 17:22:39 +0900

    [추가] 메모이제이션 패턴

--- client/lib/utils/index.js
+++ client/lib/utils/index.js
@@ -1,4 +1,3 @@
-
-
-export * from './copy.js'
-export * from './typeOf.js'
+export * from './copy.js';
+export * from './memo.js';
+export * from './typeOf.js';
--- client/main.js
+++ client/main.js
@@ -1,4 +1,16 @@
-import { diceAnimation, getNode, getNodes } from './lib/index.js';
+import {
+  diceAnimation,
+  disableElement,
+  enableElement,
+  getNode,
+  clearContents,
+  getNodes,
+  insertLast,
+  visibleElement,
+  invisibleElement,
+  memo,
+  attr,
+} from './lib/index.js';
 
 // [ 주사위 굴리기 ]
 // 1. dice 애니메이션 불러오기
@@ -13,21 +25,57 @@ import { diceAnimation, getNode, getNodes } from './lib/index.js';
 // 4. visible 활성 유틸 함수 만들기
 // 5. toggleState 유틸 함수 만들기
 
+// [ 레코드 템플릿 뿌리기 ]
+// 1. renderRecordListItem 함수 만들기
+// 2. HTML 템플릿 만들기
+// 3. 템플릿 뿌리기
+
+// [ 초기화 시키기 ]
+// 1. clearContent 로 정보 지우기
+// 2. total, count 초기화
+// 3. 스크롤 밑으로 보내기
+// 4. 메모이제이션 패턴
+
 // 배열의 구조 분해 할당
 const [rollingDiceButton, recordButton, resetButton] = getNodes(
   '.buttonGroup > button'
 );
 
-// console.log(button);
-// const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
-// const recordButton = getNode('.buttonGroup > button:nth-child(2)');
-// const resetButton = getNode('.buttonGroup > button:nth-child(3)');
+const recordListWrapper = getNode('.recordListWrapper');
 
-// let stopAnimation;
+memo('@tbody', () => getNode('.recordListWrapper tbody'));
 
-// IIFE
+// 특정 대상의 속성값을 가져오거나 / 설정할 수 있는 함수
 
-const handlerRollingDice = (() => {
+/* -------------------------------------------------------------------------- */
+/* render                                                                     */
+/* -------------------------------------------------------------------------- */
+
+let count = 0;
+let total = 0;
+// redux
+// mobx
+
+function renderRecordListItem() {
+  let diceValue = Number(attr(memo('cube'), 'data-dice'));
+
+  let template = /* html */ `
+    <tr>
+      <td>${++count}</td>
+      <td>${diceValue}</td>
+      <td>${(total += diceValue)}</td>
+    </tr>
+  `;
+
+  insertLast(memo('@tbody'), template);
+  recordListWrapper.scrollTop = recordListWrapper.scrollHeight;
+}
+
+/* -------------------------------------------------------------------------- */
+/* event                                                                      */
+/* -------------------------------------------------------------------------- */
+
+const handleRollingDice = (() => {
   let isRolling = false;
   let stopAnimation;
 
@@ -35,17 +83,35 @@ const handlerRollingDice = (() => {
     if (!isRolling) {
       // console.log('첫번째 클릭');
       stopAnimation = setInterval(diceAnimation, 100);
-      recordButton.disabled = true;
+
+      disableElement(recordButton);
+      disableElement(resetButton);
     } else {
       // console.log('두번째 클릭');
       clearInterval(stopAnimation);
-      recordButton.disabled = false;
+      enableElement(recordButton);
+      enableElement(resetButton);
     }
 
     isRolling = !isRolling;
   };
 })();
 
-rollingDiceButton.addEventListener('click', handlerRollingDice);
+const handleRecord = () => {
+  visibleElement(recordListWrapper);
+  renderRecordListItem();
+};
+
+const handleReset = () => {
+  count = 0;
+  total = 0;
+
+  invisibleElement(recordListWrapper);
+  clearContents(memo('@tbody'));
+};
+
+rollingDiceButton.addEventListener('click', handleRollingDice);
+recordButton.addEventListener('click', handleRecord);
+resetButton.addEventListener('click', handleReset);
 
 // let eventOff = bindEvent(rollingDiceButton,'click',handlerRollingDice);
