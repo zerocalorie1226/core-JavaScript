547629083583a91d8fc5becff8889d19826b5e2f
Sungchan CHo <kjyy07@naver.com>
2023-01-27 17:17:14 +0900

    [추가]

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,11 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: 'eslint:recommended',
+  extends: "eslint:recommended",
   overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {},
 };
